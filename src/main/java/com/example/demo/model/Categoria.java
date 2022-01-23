6aba23aed10d3d468ccc18eb052917df7c0fe909
Carlos Alexandre <carlosalexanadre5670@gmail.com>
2022-01-23 16:03:32 -0300

    Exception handling in POST categories - #3/25

--- src/main/java/com/example/demo/model/Categoria.java
+++ src/main/java/com/example/demo/model/Categoria.java
@@ -5,34 +5,24 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.EqualsAndHashCode;
-//import lombok.Getter;
-//import lombok.Setter;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-//@Getter
-//@Setter
+@Getter
+@Setter
 @EqualsAndHashCode
 @Table(name = "categoria")
 public class Categoria {
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //Gerado apartir da tebela
 	private Long codigo;
+	
+	@NotNull
+	@Size(min = 3, max = 30)
 	private String nome;
-	public Long getCodigo() {
-		return codigo;
-	}
-	public void setCodigo(Long codigo) {
-		this.codigo = codigo;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	
 }
