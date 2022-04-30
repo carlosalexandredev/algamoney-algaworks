180930382e290d216660a981133d6a9c978e705d
Carlos Alexandre <carlosalexanadre5670@gmail.com>
2022-04-30 19:07:01 -0300

    Implementing OAuth2 and JWT Authentication - #6/25

---
+++ src/main/java/com/example/demo/config/AuthorizationServerConfig.java
@@ -0,0 +1,67 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+			.withClient("angular")
+			.secret(passwordEncoder.encode("@ngul@r0"))
+			.scopes("read", "write")
+			.authorizedGrantTypes("password")
+			.accessTokenValiditySeconds(1800);
+		
+//		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//		String password = "{noop}admin";
+//		String encodedPassword = passwordEncoder.encode(password);
+//
+//		System.out.println();
+//		System.out.println("Password is         : " + password);
+//		System.err.println("Encoded Password is : " + encodedPassword);
+//		System.out.println();
+//		boolean isPasswordMatch = passwordEncoder.matches(password, encodedPassword);
+//		System.out.println("Password : " + password + "   isPasswordMatch    : " + isPasswordMatch);
+		
+	}
+	
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints
+		.tokenStore(tokenStore())
+		.accessTokenConverter(accessTokenConverter())
+		.authenticationManager(authenticationManager);
+	}
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("algawork");
+		return accessTokenConverter;
+	}
+
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+}
