fcfcdfde9e8e2fb16346ad704789aa87e179f0e2
carlosalexandredev <carlosalexandre5670@gmail.com>
2022-05-01 18:42:40 -0300

    refactor: Adjusting OAuth2 and JWT Authentication - #6/25

--- src/main/java/com/example/demo/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/demo/config/AuthorizationServerConfig.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -28,20 +30,21 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		clients.inMemory()
 			.withClient("angular")
 			.secret(passwordEncoder.encode("@ngul@r0"))
+//			.secret("@ngul@r0")
 			.scopes("read", "write")
 			.authorizedGrantTypes("password")
 			.accessTokenValiditySeconds(1800);
 		
-//		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//		String password = "{noop}admin";
-//		String encodedPassword = passwordEncoder.encode(password);
-//
-//		System.out.println();
-//		System.out.println("Password is         : " + password);
-//		System.err.println("Encoded Password is : " + encodedPassword);
-//		System.out.println();
-//		boolean isPasswordMatch = passwordEncoder.matches(password, encodedPassword);
-//		System.out.println("Password : " + password + "   isPasswordMatch    : " + isPasswordMatch);
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String password = "@ngul@r0";
+		String encodedPassword = passwordEncoder.encode(password);
+
+		System.out.println();
+		System.out.println("Password is         : " + password);
+		System.err.println("Encoded Password is : " + encodedPassword);
+		System.out.println();
+		boolean isPasswordMatch = passwordEncoder.matches(password, encodedPassword);
+		System.out.println("Password : " + password + "   isPasswordMatch    : " + isPasswordMatch);
 		
 	}
 	
@@ -50,18 +53,21 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints
 		.tokenStore(tokenStore())
-		.accessTokenConverter(accessTokenConverter())
+//		.accessTokenConverter(accessTokenConverter())
 		.authenticationManager(authenticationManager);
 	}
-	@Bean
-	public JwtAccessTokenConverter accessTokenConverter() {
-		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
-		accessTokenConverter.setSigningKey("algawork");
-		return accessTokenConverter;
-	}
+	
+//	@Bean
+//	public JwtAccessTokenConverter accessTokenConverter() {
+//		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+//		accessTokenConverter.setSigningKey("algawork");
+//		return accessTokenConverter;
+//	}
 
 	@Bean
 	public TokenStore tokenStore() {
-		return new JwtTokenStore(accessTokenConverter());
+//		return new JwtTokenStore(accessTokenConverter());
+		return new InMemoryTokenStore();
+
 	}
 }
